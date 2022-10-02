57318a12af816d97eafd5e9dbcd09e55fe2a7fac
ChrisJonesTC <cfjones91@gmail.com>
2022-10-02 12:03:34 -0400

    initial commit

---
+++ LINQ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string>() {"God Of War", "Ark: Survival Evolved", "Assasins Creed", "Red Dead Redemption" };
+            videoGames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
